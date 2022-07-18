f528e570155fa11bbe7088d7386d3f7752723bc2
Carlos A. Herrera <cherrera1208@gmail.com>
2022-07-17 19:44:42 -0700

    question 4 FAILS'

--- js/app.js
+++ js/app.js
@@ -89,12 +89,13 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let saSum = (sum(sumArr));
+  let saMSG = (`${testArray} was passed in as an array of numbers, and ${saSum} is their sum`);
+  console.log(saSum, saMSG);
 }
-
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -114,7 +115,7 @@ function multiplyArray(multArr) { //eslint-disable-line
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
