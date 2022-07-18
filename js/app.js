0f93c438e9bc8d5e618e4a8b294867f983571272
Carlos A. Herrera <cherrera1208@gmail.com>
2022-07-17 19:53:41 -0700

    test 5 FAILS. Didn't save work from q4, saved in this branch

--- js/app.js
+++ js/app.js
@@ -89,12 +89,14 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+  let saMSG = (`${testArray} was passed in as an array of numbers, and ###### is their sum`);
+  console.log(saMSG);
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -110,11 +112,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  let maMSG = (`The numbers ${testArray} have a product of #######`);
+  console.log(maMSG);
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
